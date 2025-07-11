08b5b80f3d92eb0e5a79220fb40225e016537a26
Giovanni Castiglioni <g.castiglioni@ug.uchile.cl>
2025-07-11 18:00:18 -0400

    Added FLAIR-1 dataset

---
+++ cropcon/datasets/flair.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import rasterio
+import torch
+
+from cropcon.datasets.base import RawGeoFMDataset
+
+class FLAIR(RawGeoFMDataset):
+    def __init__(
+        self,
+        split: str,
+        dataset_name: str,
+        multi_modal: bool,
+        multi_temporal: int,
+        support_test: bool,
+        root_path: str,
+        classes: list,
+        num_classes: int,
+        ignore_index: int,
+        img_size: int,
+        bands: dict[str, list[str]],
+        distribution: list[int],
+        data_mean: dict[str, list[str]],
+        data_std: dict[str, list[str]],
+        data_min: dict[str, list[str]],
+        data_max: dict[str, list[str]],
+        download_url: str,
+        auto_download: bool,
+        fold_config: int
+    ):
+        """Initialize the PASTIS dataset.
+
+        Args:
+            split (str): split of the dataset (train, val).
+            dataset_name (str): dataset name.
+            multi_modal (bool): if the dataset is multi-modal.
+            multi_temporal (int): number of temporal frames.
+            root_path (str): root path of the dataset.
+            classes (list): classes of the dataset.
+            num_classes (int): number of classes.
+            ignore_index (int): index to ignore for metrics and loss.
+            img_size (int): size of the image.
+            bands (dict[str, list[str]]): bands of the dataset.
+            distribution (list[int]): class distribution.
+            data_mean (dict[str, list[str]]): mean for each band for each modality.
+            Dictionary with keys as the modality and values as the list of means.
+            e.g. {"s2": [b1_mean, ..., bn_mean], "s1": [b1_mean, ..., bn_mean]}
+            data_std (dict[str, list[str]]): str for each band for each modality.
+            Dictionary with keys as the modality and values as the list of stds.
+            e.g. {"s2": [b1_std, ..., bn_std], "s1": [b1_std, ..., bn_std]}
+            data_min (dict[str, list[str]]): min for each band for each modality.
+            Dictionary with keys as the modality and values as the list of mins.
+            e.g. {"s2": [b1_min, ..., bn_min], "s1": [b1_min, ..., bn_min]}
+            data_max (dict[str, list[str]]): max for each band for each modality.
+            Dictionary with keys as the modality and values as the list of maxs.
+            e.g. {"s2": [b1_max, ..., bn_max], "s1": [b1_max, ..., bn_max]}
+            download_url (str): url to download the dataset.
+            auto_download (bool): whether to download the dataset automatically.
+            fold_config (int): configuration of folds to split the data
+        """
+        super(FLAIR, self).__init__(
+            split=split,
+            dataset_name=dataset_name,
+            multi_modal=multi_modal,
+            multi_temporal=multi_temporal,
+            support_test=support_test,
+            root_path=root_path,
+            classes=classes,
+            num_classes=num_classes,
+            ignore_index=ignore_index,
+            img_size=img_size,
+            bands=bands,
+            distribution=distribution,
+            data_mean=data_mean,
+            data_std=data_std,
+            data_min=data_min,
+            data_max=data_max,
+            download_url=download_url,
+            auto_download=auto_download,
+            fold_config=fold_config
+        )
+
+        self.paths = pd.read_csv(f"{root_path}/csv_full/flair-1-paths-{split}.csv", header=None)
+
+    def __getitem__(self, i: int) -> dict[str, torch.Tensor | dict[str, torch.Tensor]]:
+        """Get the item at index i.
+
+        Args:
+            i (int): index of the item.
+
+        Returns:
+            dict[str, torch.Tensor | dict[str, torch.Tensor]]: output dictionary follwing the format
+            {"image": {"optical": torch.Tensor},
+            "target": torch.Tensor}.
+        """
+        img_path = self.root_path + (self.paths.iloc[i, 0].split("data")[1] if self.split == "test" else self.paths.iloc[i, 0][2:])
+        label_path = self.root_path + (self.paths.iloc[i, 1].split("data")[1] if self.split == "test" else self.paths.iloc[i, 1][2:])
+
+        output = {}
+
+        with rasterio.open(img_path) as f:
+            output["image"] = {"optical": torch.FloatTensor(f.read()).unsqueeze(1)}
+
+        with rasterio.open(label_path) as f:
+            output["target"] = torch.LongTensor(f.read()).squeeze()
+
+        return output
+
+    def __len__(self) -> int:
+        """Return the length of the dataset.
+
+        Returns:
+            int: length of the dataset.
+        """
+        return len(self.paths)
+
+    @staticmethod
+    def download():
+        pass
