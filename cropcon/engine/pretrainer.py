0ccddfbf61edd3af358ea40786bb2d840a9481cc
Giovanni Castiglioni <g.castiglioni@ug.uchile.cl>
2025-09-24 09:28:10 -0300

    Pretrainer checkpoint is just the model

--- cropcon/engine/pretrainer.py
+++ cropcon/engine/pretrainer.py
@@ -257,6 +257,7 @@ class Trainer:
             )
         return batch_loss
 
+    @torch.no_grad()
     def temporal_transform(self, x: torch.Tensor, mask: torch.Tensor):
         """
         x:     [B, C, T, H, W]
@@ -322,6 +323,7 @@ class Trainer:
             torch.distributed.barrier(device_ids=[torch.cuda.current_device()])
             return
         checkpoint = self.get_checkpoint(epoch) if checkpoint is None else checkpoint
+        checkpoint = checkpoint["model"]
         suffix = "_best" if is_best else f"{epoch}_final" if is_final else f"{epoch}"
         checkpoint_path = os.path.join(self.exp_dir, f"checkpoint_{suffix}.pth")
         torch.save(checkpoint, checkpoint_path)
