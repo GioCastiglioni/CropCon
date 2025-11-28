d2a7dd6e422e984c9e3c848a43bcf67ffff9b073
Giovanni Castiglioni <g.castiglioni@ug.uchile.cl>
2025-11-28 14:41:15 -0300

    Implemented LeJEPA4SITS

--- cropcon/engine/pretrainer.py
+++ cropcon/engine/pretrainer.py
@@ -52,7 +52,6 @@ class Trainer:
         """
         self.rank = int(os.environ["RANK"])
         self.criterion = criterion
-        self.logit_compensation = str(self.criterion) == "LogitCompensation"
         self.model = model
         self.teacher = teacher
         self.teacher.eval()
@@ -69,6 +68,7 @@ class Trainer:
         self.ckpt_interval = ckpt_interval
         self.eval_interval = eval_interval
         self.log_interval = log_interval
+        self.n_classes = self.model.module.num_classes
 
         self.training_stats = {
             name: RunningAverageMeter(length=self.batch_per_epoch)
@@ -97,16 +97,9 @@ class Trainer:
         
         self.transform = ConsistentTransform(h_w=self.model.module.encoder.input_size, degrees=45).to(self.device)
 
-        self.n_classes = self.model.module.num_classes
+        self.n_global = 2
+        self.n_local = 8
 
-        self.ema_momentum = 0.996
-
-        self.grid_size = self.criterion.grid_size[0]
-        self.n_target_blocks = 4  # Número de BLOQUES target
-        self.target_h = 3         # Altura de un bloque target
-        self.target_w = 2         # Ancho de un bloque target
-        self.context_h = 7        # Altura del bloque de contexto (ej. ~75%)
-        self.context_w = 7        # Ancho del bloque de contexto
     
     def train(self) -> None:
         """Train the model for n_epochs then evaluate the model and save the best model."""
@@ -147,70 +140,29 @@ class Trainer:
         end_time = time.time()
         for batch_idx, data in enumerate(self.train_loader):
 
-            image = self.temporal_transform(data["image"]["optical"].to(self.device))
-            B, C, T, H, W = image.shape
+            views = []
+            for _ in range(self.n_global):
+                views.append(self.temporal_transform(data["image"]["optical"].to(self.device)))
 
-            mask_token = self.model.module.mask_token.unsqueeze(2).expand(B, -1, T, -1, -1)
-
-            N_PATCHES = self.grid_size**2
-            PATCH_SIZE = H // self.grid_size
+            B, C, T, H, W = views[0].shape
             
-            mask_ctx_1d = torch.zeros(B, N_PATCHES, device=self.device, dtype=torch.bool)
-            mask_tgt_1d = torch.zeros(B, N_PATCHES, device=self.device, dtype=torch.bool)
-
-            for b in range(B):
-                top_t = torch.randint(0, self.grid_size - self.target_h + 1, (self.n_target_blocks,), device=self.device)
-                left_t = torch.randint(0, self.grid_size - self.target_w + 1, (self.n_target_blocks,), device=self.device)
-
-                mask_tgt_2d = torch.zeros(self.grid_size, self.grid_size, device=self.device, dtype=torch.bool)
-                
-                idx_target_batch = []
-                for i in range(self.n_target_blocks):
-                    y_coords = torch.arange(top_t[i], top_t[i] + self.target_h, device=self.device).view(-1, 1)
-                    x_coords = torch.arange(left_t[i], left_t[i] + self.target_w, device=self.device).view(1, -1)
-                    
-                    block_indices_2d = (y_coords * self.grid_size + x_coords).flatten()
-                    
-                    mask_tgt_2d.view(-1)[block_indices_2d] = True
-                    idx_target_batch.append(block_indices_2d)
-                
-                mask_tgt_1d[b] = mask_tgt_2d.flatten()
-
-                top_c = torch.randint(0, self.grid_size - self.context_h + 1, (1,), device=self.device).item()
-                left_c = torch.randint(0, self.grid_size - self.context_w + 1, (1,), device=self.device).item()
-                
-                mask_ctx_2d = torch.zeros(self.grid_size, self.grid_size, device=self.device, dtype=torch.bool)
-                mask_ctx_2d[top_c:top_c+self.context_h, left_c:left_c+self.context_w] = True
-                
-                mask_ctx_final_2d = mask_ctx_2d & (~mask_tgt_2d)
-                
-                mask_ctx_1d[b] = mask_ctx_final_2d.flatten()
-            
-            mask_ctx = mask_ctx_1d.view(B, self.grid_size, self.grid_size)
-            
-            mask_ctx_pixel = mask_ctx.repeat_interleave(PATCH_SIZE, dim=1).repeat_interleave(PATCH_SIZE, dim=2)
-
-            mask_ctx_5d = mask_ctx_pixel.unsqueeze(1).unsqueeze(2).expand_as(image)
-            
-            image_context = torch.where(mask_ctx_5d, image, mask_token)
-            
-            image_target = image
             
             self.training_stats["data_time"].update(time.time() - end_time)
 
             with torch.autocast(
                 "cuda", enabled=self.enable_mixed_precision, dtype=self.precision
             ):
-                feat_student = self.model.module.forward_features(image_context, batch_positions=data["metadata"])
-                feat_student = self.model.module.patch_conv(feat_student)
-
-                with torch.no_grad():
-                    feat_teacher = self.teacher.module.forward_features(image_target, batch_positions=data["metadata"])
-                    feat_teacher = self.teacher.module.patch_conv(feat_teacher)
-
-                loss_dict = self.compute_loss(feat_student, feat_teacher, mask_ctx_1d, mask_tgt_1d)
-
-                loss = loss_dict["loss"]
+                global_views = []
+                local_views = []
+                local_indexes = torch.linspace(0, T-1, self.n_local//self.n_global).long()
+                for view in views:
+                    out, feature_maps, _, _ = self.model.module.encoder(view, batch_positions=data["metadata"])
+                    out = self.model.module.encoder.projector(out)
+                    global_views.append(out)
+                    for local_index in local_indexes:
+                        local_views.append(self.model.module.encoder.projector(feature_maps[-1][:,local_index,:,:,:]))
+                
+                loss = self.compute_loss(global_views, local_views)
 
                 if loss == 0.0:
                     loss = loss + 0.0 * sum(p.sum() for p in self.model.module.parameters())
@@ -226,8 +178,6 @@ class Trainer:
             self.scaler.step(self.optimizer)
             self.scaler.update()
 
-            self._update_teacher_ema(epoch, batch_idx)
-
             self.training_stats['loss'].update(loss.item())
             if (batch_idx + 1) % self.log_interval == 0:
                 self.log(batch_idx + 1, epoch)
@@ -235,19 +185,11 @@ class Trainer:
             self.lr_scheduler.step()
 
             if self.use_wandb and self.rank == 0:
-                with torch.no_grad():
-                    feat_student_std = feat_student.std()
-                    feat_teacher_std = feat_teacher.std()
 
                 self.wandb.log(
                     {
                         "train_loss": loss.item(),
-                        "train_student_std": feat_student_std.item(),
-                        "train_teacher_std": feat_teacher_std.item(),
                         "learning_rate": self.optimizer.param_groups[0]["lr"],
-                        "train_mse": loss_dict["mse_loss"].item(),
-                        "train_var": loss_dict["var_loss"].item(),
-                        "train_cov": loss_dict["cov_loss"].item(),
                         "epoch": epoch,
                         **{
                             f"train_{k}": v.avg
@@ -272,100 +214,36 @@ class Trainer:
         self.model.eval()
 
         total_epoch_loss = 0.0
-        total_student_std = 0.0
-        total_teacher_std = 0.0
-        mse_epoch_loss = 0.0
-        var_epoch_loss = 0.0
-        cov_epoch_loss = 0.0
         
         end_time = time.time()
         for batch_idx, data in enumerate(self.val_loader):
 
             image = data["image"]["optical"].to(self.device)
             B, C, T, H, W = image.shape
-            mask_token = self.model.module.mask_token.unsqueeze(2).expand(B, -1, T, -1, -1)
-
-            N_PATCHES = self.grid_size**2
-            PATCH_SIZE = H // self.grid_size
-            
-            mask_ctx_1d = torch.zeros(B, N_PATCHES, device=self.device, dtype=torch.bool)
-            mask_tgt_1d = torch.zeros(B, N_PATCHES, device=self.device, dtype=torch.bool)
-
-            for b in range(B):
-                top_t = torch.randint(0, self.grid_size - self.target_h + 1, (self.n_target_blocks,), device=self.device)
-                left_t = torch.randint(0, self.grid_size - self.target_w + 1, (self.n_target_blocks,), device=self.device)
-
-                mask_tgt_2d = torch.zeros(self.grid_size, self.grid_size, device=self.device, dtype=torch.bool)
-                
-                idx_target_batch = []
-                for i in range(self.n_target_blocks):
-                    y_coords = torch.arange(top_t[i], top_t[i] + self.target_h, device=self.device).view(-1, 1)
-                    x_coords = torch.arange(left_t[i], left_t[i] + self.target_w, device=self.device).view(1, -1)
-                    
-                    block_indices_2d = (y_coords * self.grid_size + x_coords).flatten()
-                    
-                    mask_tgt_2d.view(-1)[block_indices_2d] = True
-                    idx_target_batch.append(block_indices_2d)
-                
-                mask_tgt_1d[b] = mask_tgt_2d.flatten()
-
-                top_c = torch.randint(0, self.grid_size - self.context_h + 1, (1,), device=self.device).item()
-                left_c = torch.randint(0, self.grid_size - self.context_w + 1, (1,), device=self.device).item()
-                
-                mask_ctx_2d = torch.zeros(self.grid_size, self.grid_size, device=self.device, dtype=torch.bool)
-                mask_ctx_2d[top_c:top_c+self.context_h, left_c:left_c+self.context_w] = True
-                
-                mask_ctx_final_2d = mask_ctx_2d & (~mask_tgt_2d)
-                
-                mask_ctx_1d[b] = mask_ctx_final_2d.flatten()
-
-            mask_ctx = mask_ctx_1d.view(B, self.grid_size, self.grid_size)
-            
-            mask_ctx_pixel = mask_ctx.repeat_interleave(PATCH_SIZE, dim=1).repeat_interleave(PATCH_SIZE, dim=2)
-
-            mask_ctx_5d = mask_ctx_pixel.unsqueeze(1).unsqueeze(2).expand_as(image)
-            
-            image_context = torch.where(mask_ctx_5d, image, mask_token)
-            image_target = image 
 
             self.training_stats["data_time"].update(time.time() - end_time)
 
             with torch.autocast(
                 "cuda", enabled=self.enable_mixed_precision, dtype=self.precision
             ):
-                feat_student = self.model.module.forward_features(image_context, batch_positions=data["metadata"])
-                feat_student = self.model.module.patch_conv(feat_student)
 
-                feat_teacher = self.teacher.module.forward_features(image_target, batch_positions=data["metadata"])
-                feat_teacher = self.teacher.module.patch_conv(feat_teacher)
+                local_indexes = torch.linspace(0, T-1, self.n_local//self.n_global).long()
 
-                avg_batch_loss_dict = self.compute_loss(feat_student, feat_teacher, mask_ctx_1d, mask_tgt_1d)
+                out, feature_maps, _, _ = self.model.module.encoder(image, batch_positions=data["metadata"])
+                global_views = [self.model.module.encoder.projector(out)]
+                local_views = [self.model.module.encoder.projector(feature_maps[0][:,local_index,:,:,:]) for local_index in local_indexes]
 
-            total_epoch_loss += avg_batch_loss_dict["loss"].item()
-            mse_epoch_loss += avg_batch_loss_dict["mse_loss"].item()
-            var_epoch_loss += avg_batch_loss_dict["var_loss"].item()
-            cov_epoch_loss += avg_batch_loss_dict["cov_loss"].item()
-            
-            total_student_std += feat_student.std()
-            total_teacher_std += feat_teacher.std()
+                batch_loss = self.compute_loss(global_views, local_views)
+
+            total_epoch_loss += batch_loss.item()
             torch.distributed.barrier(device_ids=[torch.cuda.current_device()])
 
         final_val_loss = total_epoch_loss / len(self.val_loader)
-        final_mse = mse_epoch_loss / len(self.val_loader)
-        final_var = var_epoch_loss / len(self.val_loader)
-        final_cov = cov_epoch_loss / len(self.val_loader)
-        final_student_std = total_student_std / len(self.val_loader)
-        final_teacher_std = total_teacher_std / len(self.val_loader)
 
         if self.use_wandb and self.rank == 0:
             self.wandb.log(
                 {
                     "val_loss": final_val_loss,
-                    "val_student_std": final_student_std,
-                    "val_teacher_std": final_teacher_std,
-                    "val_mse": final_mse,
-                    "val_var": final_var,
-                    "val_cov": final_cov,
                     "epoch": epoch
                 },
                 step = epoch * len(self.train_loader)
@@ -396,29 +274,6 @@ class Trainer:
             x_out[b] = x_b
 
         return x_out
-
-    @torch.no_grad()
-    def _update_teacher_ema(self, epoch: int, batch_idx: int):
-        
-        current_step = epoch * self.batch_per_epoch + batch_idx
-        total_steps = self.batch_per_epoch * self.n_epochs
-        m = self.ema_momentum + (1 - self.ema_momentum) * (current_step / total_steps)
-
-        student_params = dict(self.model.module.named_parameters())
-        teacher_params = dict(self.teacher.module.named_parameters())
-
-        for name, student_param in student_params.items():
-            if name in teacher_params:
-                teacher_param = teacher_params[name]
-                teacher_param.data.mul_(m).add_(student_param.data, alpha=1 - m)
-        
-        student_buffers = dict(self.model.module.named_buffers())
-        teacher_buffers = dict(self.teacher.module.named_buffers())
-
-        for name, student_buffer in student_buffers.items():
-            if name in teacher_buffers:
-                teacher_buffer = teacher_buffers[name]
-                teacher_buffer.data.copy_(student_buffer.data)
 
     def get_checkpoint(self, epoch: int) -> dict[str, dict | int]:
         """Create a checkpoint dictionary, containing references to the pytorch tensors.
